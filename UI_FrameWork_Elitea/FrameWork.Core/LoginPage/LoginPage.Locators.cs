e55de3741d98224118b812494952b285836452c7
LokeshGurram172 <gurramlokesh02@gmail.com>
2024-11-13 11:33:21 +0530

    Update LoginPage.Locators.cs

--- UI_FrameWork_Elitea/FrameWork.Core/LoginPage/LoginPage.Locators.cs
+++ UI_FrameWork_Elitea/FrameWork.Core/LoginPage/LoginPage.Locators.cs
@@ -8,5 +8,10 @@ namespace FrameWork.Core.LoginPage
 {
     partial class LoginPage
     {
+         private readonly By _userName = By.Id("user-name");
+         private readonly By _password = By.Id("password");
+         private readonly By _loginbtn = RelativeBy.WithLocator(By.TagName("input")).Below(By.Id("password"));
+         private readonly By _errorMsg = By.XPath("//button[@class='error-button']");
+         private readonly By _productPage = By.XPath("//span[text()='Products']");
     }
 }
