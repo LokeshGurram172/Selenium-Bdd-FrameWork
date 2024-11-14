705d973c50c56c3b2a2fd90db1430023b04e5e90
LokeshGurram172 <gurramlokesh02@gmail.com>
2024-11-14 11:03:42 +0530

    Create UI_FrameWork_Elitea/AutomationFrameworkUtilities/WebDriverUtilities.cs

---
+++ UI_FrameWork_Elitea/AutomationFrameworkUtilities/WebDriverUtilities.cs
@@ -0,0 +1,39 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace AutomationFrameworkUtilities
+{
+    public static class WebDriverUtilities
+    {
+        public static IWebDriver InitializeDriver(string browserType)
+        {
+            IWebDriver driver;
+            switch (browserType.ToLower())
+            {
+                case "chrome":
+                    driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+                    break;
+                case "firefox":
+                    driver = new OpenQA.Selenium.Firefox.FirefoxDriver();
+                    break;
+                case "edge":
+                    driver = new OpenQA.Selenium.Edge.EdgeDriver();
+                    break;
+                default:
+                    throw new ArgumentException("Browser not supported");
+            }
+            return driver;
+        }
+
+        public static void NavigateToUrl(IWebDriver driver, string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public static void CloseDriver(IWebDriver driver)
+        {
+            driver.Quit();
+        }
+    }
+}
