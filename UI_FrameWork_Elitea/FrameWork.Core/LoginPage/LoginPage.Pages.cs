0c3bb0ed20e31812a9e5a8ef86bffd99ee6bca5e
LokeshGurram172 <gurramlokesh02@gmail.com>
2024-11-15 11:43:08 +0530

    Update UI_FrameWork_Elitea/FrameWork.Core/LoginPage/LoginPage.Pages.cs

--- UI_FrameWork_Elitea/FrameWork.Core/LoginPage/LoginPage.Pages.cs
+++ UI_FrameWork_Elitea/FrameWork.Core/LoginPage/LoginPage.Pages.cs
@@ -8,5 +8,29 @@ namespace FrameWork.Core.LoginPage
 {
     partial class LoginPage
     {
+        public void EnterUsername(string username)
+        {
+            DriverUtility.EnterText(_userName, username);
+        }
+
+        public void EnterPassword(string password)
+        {
+            DriverUtility.EnterText(_password, password);
+        }
+
+        public void ClickLoginButton()
+        {
+            DriverUtility.Click(_loginbtn);
+        }
+
+        public bool IsErrorMessageVisible()
+        {
+            return DriverUtility.IsElementVisible(_errorMsg);
+        }
+
+        public bool IsProductPageVisible()
+        {
+            return DriverUtility.IsElementVisible(_productPage);
+        }
     }
 }
