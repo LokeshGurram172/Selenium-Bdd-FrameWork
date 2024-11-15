97c0ca713e11659aeee58b000e3864d313da44f9
LokeshGurram172 <gurramlokesh02@gmail.com>
2024-11-15 12:15:06 +0530

    Update UI_FrameWork_Elitea/SpecFlowTest/Hooks/Hooks.cs

--- UI_FrameWork_Elitea/SpecFlowTest/Hooks/Hooks.cs
+++ UI_FrameWork_Elitea/SpecFlowTest/Hooks/Hooks.cs
@@ -1,10 +1,31 @@
 ﻿using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using AutomationFrameworkUtilities;
 
 namespace SpecFlowTest.Hooks
 {
     [Binding]
     public sealed class Hooks
     {
-       
+        private readonly IObjectContainer _objectContainer;
+        private IWebDriver _driver;
+
+        public Hooks(IObjectContainer objectContainer)
+        {
+            _objectContainer = objectContainer;
+        }
+
+        [BeforeScenario("@login", "@invalid_login")]
+        public void BeforeScenario()
+        {
+            _driver = WebDriverUtility.InitializeDriver("chrome");
+            _objectContainer.RegisterInstanceAs(_driver);
+        }
+
+        [AfterScenario("@login", "@invalid_login")]
+        public void AfterScenario()
+        {
+            WebDriverUtility.TearDown(_driver);
+        }
     }
 }
