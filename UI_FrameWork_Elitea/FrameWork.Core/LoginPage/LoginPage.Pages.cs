8cd15f5e3caa106111bbb0978e15e6110e4be3c9
LokeshGurram172 <gurramlokesh02@gmail.com>
2024-11-15 14:06:40 +0530

    Update UI_FrameWork_Elitea/FrameWork.Core/LoginPage/LoginPage.Pages.cs

--- UI_FrameWork_Elitea/FrameWork.Core/LoginPage/LoginPage.Pages.cs
+++ UI_FrameWork_Elitea/FrameWork.Core/LoginPage/LoginPage.Pages.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OpenQA.Selenium;
+using AutomationFrameWorkUtilities;
 
 namespace FrameWork.Core.LoginPage
 {
-    partial class LoginPage
+    public partial class LoginPage
     {
+        private IWebDriver driver;
+        private static readonly LoginPageLocators Locators = new LoginPageLocators();
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void EnterCredentials(string username, string password)
+        {
+            driver.FindElement(Locators.UsernameField).SendKeys(username);
+            driver.FindElement(Locators.PasswordField).SendKeys(password);
+        }
+
+        public void ClickLoginButton()
+        {
+            driver.FindElement(Locators.LoginButton).Click();
+        }
+
+        public string GetErrorMessage()
+        {
+            return driver.FindElement(Locators.ErrorMessage).Text;
+        }
     }
 }
