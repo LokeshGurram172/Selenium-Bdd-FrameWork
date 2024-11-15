4be6932d99b9a807e66ff7ba70a10b52e0608809
LokeshGurram172 <gurramlokesh02@gmail.com>
2024-11-15 12:07:15 +0530

    Update UI_FrameWork_Elitea/SpecFlowTest/StepDefinitions/LoginPageStepDefinitions.cs

--- UI_FrameWork_Elitea/SpecFlowTest/StepDefinitions/LoginPageStepDefinitions.cs
+++ UI_FrameWork_Elitea/SpecFlowTest/StepDefinitions/LoginPageStepDefinitions.cs
@@ -3,6 +3,40 @@ namespace SpecFlowTest.StepDefinitions
     [Binding]
     public sealed class LoginPageStepDefinitions
     {
-       
+        private readonly LoginPage _loginPage;
+
+        public LoginPageStepDefinitions(LoginPage loginPage)
+        {
+            _loginPage = loginPage;
+        }
+
+        [Given("I am on the Swag Labs login page")]
+        public void GivenIAmOnTheSwagLabsLoginPage()
+        {
+            // Navigation logic here
+        }
+
+        [When("I enter valid credentials")]
+        [When("I enter invalid credentials")]
+        public void WhenIEnterCredentials(Table table)
+        {
+            var username = table.Rows[0]["username"];
+            var password = table.Rows[0]["password"];
+            _loginPage.EnterUsername(username);
+            _loginPage.EnterPassword(password);
+            _loginPage.ClickLoginButton();
+        }
+
+        [Then("I should be redirected to the products page")]
+        public void ThenIShouldBeRedirectedToTheProductsPage()
+        {
+            // Assertion logic here
+        }
+
+        [Then("an error message should be displayed")]
+        public void ThenAnErrorMessageShouldBeDisplayed()
+        {
+            // Assertion logic here
+        }
     }
 }
