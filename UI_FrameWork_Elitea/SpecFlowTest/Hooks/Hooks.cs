2221a2687ac7d4a0c8d0587298c943f9b4e2e69c
LokeshGurram172 <gurramlokesh02@gmail.com>
2024-11-18 12:08:30 +0530

    Update UI_FrameWork_Elitea/SpecFlowTest/Hooks/Hooks.cs

--- UI_FrameWork_Elitea/SpecFlowTest/Hooks/Hooks.cs
+++ UI_FrameWork_Elitea/SpecFlowTest/Hooks/Hooks.cs
@@ -6,5 +6,33 @@ namespace SpecFlowTest.Hooks
     public sealed class Hooks
     {
        
-    }
+    [BeforeScenario("Login")]
+public void BeforeLoginScenario()
+{
+	// Setup code specific to login scenarios
+	WebDriver.Initialize();
+	WebDriver.NavigateTo("https://www.saucedemo.com/");
+}
+
+[AfterScenario("Login")]
+public void AfterLoginScenario()
+{
+	// Teardown code specific to login scenarios
+	WebDriver.Quit();
+}
+}
+[BeforeScenario("Login")]
+public void BeforeLoginScenario()
+{
+	// Setup code specific to login scenarios
+	WebDriver.Initialize();
+	WebDriver.NavigateTo("https://www.saucedemo.com/");
+}
+
+[AfterScenario("Login")]
+public void AfterLoginScenario()
+{
+	// Teardown code specific to login scenarios
+	WebDriver.Quit();
+}
 }
