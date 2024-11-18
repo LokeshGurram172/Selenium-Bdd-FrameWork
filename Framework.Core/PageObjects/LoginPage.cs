3587dde0ab0320f7064a29c31fa79a39c6020e37
LokeshGurram172 <gurramlokesh02@gmail.com>
2024-11-18 11:28:54 +0530

    Create Framework.Core/PageObjects/LoginPage.cs

---
+++ Framework.Core/PageObjects/LoginPage.cs
@@ -0,0 +1,27 @@
+public class LoginPage
+{
+    public string UsernameField = "#user-name";
+    public string PasswordField = "#password";
+    public string LoginButton = "#login-button";
+    public string ErrorMessage = "div.error-message-container.error";
+
+    public void EnterUsername(string username)
+    {
+        // Code to enter username
+    }
+
+    public void EnterPassword(string password)
+    {
+        // Code to enter password
+    }
+
+    public void ClickLoginButton()
+    {
+        // Code to click login button
+    }
+
+    public string GetErrorMessage()
+    {
+        // Code to get error message
+    }
+}
