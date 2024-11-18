58ccc2aba1038b7f9b5293c76377f5e09dcdc095
LokeshGurram172 <gurramlokesh02@gmail.com>
2024-11-18 12:12:24 +0530

    Update UI_FrameWork_Elitea/SpecFlowTest/StepDefinitions/LoginPageStepDefinitions.cs

--- UI_FrameWork_Elitea/SpecFlowTest/StepDefinitions/LoginPageStepDefinitions.cs
+++ UI_FrameWork_Elitea/SpecFlowTest/StepDefinitions/LoginPageStepDefinitions.cs
@@ -4,5 +4,65 @@ namespace SpecFlowTest.StepDefinitions
     public sealed class LoginPageStepDefinitions
     {
        
-    }
+    [Given("I am on the Swag Labs login page")]
+public void GivenIAmOnTheSwagLabsLoginPage()
+{
+	// Navigate to the login page
+	LoginPage.GoTo();
+}
+
+[When("I enter valid credentials")]
+public void WhenIEnterValidCredentials()
+{
+	LoginPage.EnterCredentials("standard_user", "secret_sauce");
+}
+
+[When("I enter invalid credentials")]
+public void WhenIEnterInvalidCredentials()
+{
+	LoginPage.EnterCredentials("invalid_user", "invalid_password");
+}
+
+[Then("I should be redirected to the products page")]
+public void ThenIShouldBeRedirectedToTheProductsPage()
+{
+	Assert.IsTrue(ProductsPage.IsVisible());
+}
+
+[Then("I should see an error message 'Username and password do not match any user in this service'")]
+public void ThenIShouldSeeAnErrorMessage()
+{
+	Assert.IsTrue(LoginPage.IsErrorMessageVisible("Username and password do not match any user in this service"));
+}
+}
+[Given("I am on the Swag Labs login page")]
+public void GivenIAmOnTheSwagLabsLoginPage()
+{
+	// Navigate to the login page
+	LoginPage.GoTo();
+}
+
+[When("I enter valid credentials")]
+public void WhenIEnterValidCredentials()
+{
+	LoginPage.EnterCredentials("standard_user", "secret_sauce");
+}
+
+[When("I enter invalid credentials")]
+public void WhenIEnterInvalidCredentials()
+{
+	LoginPage.EnterCredentials("invalid_user", "invalid_password");
+}
+
+[Then("I should be redirected to the products page")]
+public void ThenIShouldBeRedirectedToTheProductsPage()
+{
+	Assert.IsTrue(ProductsPage.IsVisible());
+}
+
+[Then("I should see an error message 'Username and password do not match any user in this service'")]
+public void ThenIShouldSeeAnErrorMessage()
+{
+	Assert.IsTrue(LoginPage.IsErrorMessageVisible("Username and password do not match any user in this service"));
+}
 }
