7db6acafd926e80f9fc0c1b1275dba64ea7529a3
LokeshGurram172 <gurramlokesh02@gmail.com>
2024-11-18 12:20:36 +0530

    Update UI_FrameWork_Elitea/SpecFlowTest/StepDefinitions/LoginPageStepDefinitions.cs

--- UI_FrameWork_Elitea/SpecFlowTest/StepDefinitions/LoginPageStepDefinitions.cs
+++ UI_FrameWork_Elitea/SpecFlowTest/StepDefinitions/LoginPageStepDefinitions.cs
@@ -4,5 +4,65 @@ namespace SpecFlowTest.StepDefinitions
     public sealed class LoginPageStepDefinitions
     {
        
-    }
+    [StepDefinition("I am on the Swag Labs login page")]
+public void GivenIAmOnTheSwagLabsLoginPage()
+{
+	// Navigate to the login page
+	Driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+}
+
+[StepDefinition("I enter valid username '(.*)' and password '(.*)'")]
+public void WhenIEnterValidCredentials(string username, string password)
+{
+	LoginPage.EnterCredentials(username, password);
+}
+
+[StepDefinition("I click the login button")]
+public void AndIClickTheLoginButton()
+{
+	LoginPage.ClickLoginButton();
+}
+
+[StepDefinition("I should be redirected to the products page")]
+public void ThenIShouldBeRedirectedToTheProductsPage()
+{
+	Assert.IsTrue(Driver.Url.Contains("inventory.html"));
+}
+
+[StepDefinition("an error message should be displayed '(.*)'")]
+public void ThenAnErrorMessageShouldBeDisplayed(string message)
+{
+	Assert.IsTrue(LoginPage.GetErrorMessage().Contains(message));
+}
+}
+[StepDefinition("I am on the Swag Labs login page")]
+public void GivenIAmOnTheSwagLabsLoginPage()
+{
+	// Navigate to the login page
+	Driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+}
+
+[StepDefinition("I enter valid username '(.*)' and password '(.*)'")]
+public void WhenIEnterValidCredentials(string username, string password)
+{
+	LoginPage.EnterCredentials(username, password);
+}
+
+[StepDefinition("I click the login button")]
+public void AndIClickTheLoginButton()
+{
+	LoginPage.ClickLoginButton();
+}
+
+[StepDefinition("I should be redirected to the products page")]
+public void ThenIShouldBeRedirectedToTheProductsPage()
+{
+	Assert.IsTrue(Driver.Url.Contains("inventory.html"));
+}
+
+[StepDefinition("an error message should be displayed '(.*)'")]
+public void ThenAnErrorMessageShouldBeDisplayed(string message)
+{
+	Assert.IsTrue(LoginPage.GetErrorMessage().Contains(message));
+}
 }
