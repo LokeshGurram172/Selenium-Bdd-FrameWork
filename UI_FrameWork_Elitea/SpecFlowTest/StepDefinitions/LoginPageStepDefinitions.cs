316030363a6e67166f55810f19f4f995de753b20
LokeshGurram172 <gurramlokesh02@gmail.com>
2024-11-18 12:08:21 +0530

    Update UI_FrameWork_Elitea/SpecFlowTest/StepDefinitions/LoginPageStepDefinitions.cs

--- UI_FrameWork_Elitea/SpecFlowTest/StepDefinitions/LoginPageStepDefinitions.cs
+++ UI_FrameWork_Elitea/SpecFlowTest/StepDefinitions/LoginPageStepDefinitions.cs
@@ -4,5 +4,65 @@ namespace SpecFlowTest.StepDefinitions
     public sealed class LoginPageStepDefinitions
     {
        
-    }
+    [Given("I am on the Swag Labs login page")]
+public void GivenIAmOnTheSwagLabsLoginPage()
+{
+	// Navigate to the login page
+	LoginPage.NavigateTo();
+}
+
+[When("I enter valid username '(.*)' and password '(.*)'")]
+public void WhenIEnterValidCredentials(string username, string password)
+{
+	LoginPage.EnterCredentials(username, password);
+}
+
+[When("I enter invalid username '(.*)' and password '(.*)'")]
+public void WhenIEnterInvalidCredentials(string username, string password)
+{
+	LoginPage.EnterCredentials(username, password);
+}
+
+[Then("I should be redirected to the products page")]
+public void ThenIShouldBeRedirectedToTheProductsPage()
+{
+	Assert.IsTrue(LoginPage.IsAtProductsPage());
+}
+
+[Then("an error message should be displayed '(.*)'")]
+public void ThenAnErrorMessageShouldBeDisplayed(string message)
+{
+	Assert.IsTrue(LoginPage.IsErrorMessageDisplayed(message));
+}
+}
+[Given("I am on the Swag Labs login page")]
+public void GivenIAmOnTheSwagLabsLoginPage()
+{
+	// Navigate to the login page
+	LoginPage.NavigateTo();
+}
+
+[When("I enter valid username '(.*)' and password '(.*)'")]
+public void WhenIEnterValidCredentials(string username, string password)
+{
+	LoginPage.EnterCredentials(username, password);
+}
+
+[When("I enter invalid username '(.*)' and password '(.*)'")]
+public void WhenIEnterInvalidCredentials(string username, string password)
+{
+	LoginPage.EnterCredentials(username, password);
+}
+
+[Then("I should be redirected to the products page")]
+public void ThenIShouldBeRedirectedToTheProductsPage()
+{
+	Assert.IsTrue(LoginPage.IsAtProductsPage());
+}
+
+[Then("an error message should be displayed '(.*)'")]
+public void ThenAnErrorMessageShouldBeDisplayed(string message)
+{
+	Assert.IsTrue(LoginPage.IsErrorMessageDisplayed(message));
+}
 }
